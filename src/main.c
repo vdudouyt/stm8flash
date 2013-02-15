f9fe770fce87539181072b2e6649ebad7a7f2d0b
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2013-02-15 21:12:39 +0700

    Detach device from kernel module

--- src/main.c
+++ src/main.c
@@ -72,6 +72,11 @@ bool usb_init(programmer_t *pgm, unsigned int vid, unsigned int pid) {
 	assert(pgm->dev_handle);
 
 	libusb_free_device_list(devs, 1); //free the list, unref the devices in it
+
+	if(libusb_kernel_driver_active(pgm->dev_handle, 0) == 1) { //find out if kernel driver is attached
+		int r = libusb_detach_kernel_driver(pgm->dev_handle, 0);
+		assert(r == 0);
+	}
 	return(true);
 }
 
