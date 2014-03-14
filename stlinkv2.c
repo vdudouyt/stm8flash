c58c60369d0934dcfb5a753116bb5f31b3eaa0d7
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2014-03-14 22:49:38 +0700

    Changed condition in stlink2_wait_until_transfer_completes()

--- stlinkv2.c
+++ stlinkv2.c
@@ -203,7 +203,7 @@ int stlink2_swim_read_range(programmer_t *pgm, stm8_device_t *device, char *buff
 }
 
 void stlink2_wait_until_transfer_completes(programmer_t *pgm, stm8_device_t *device) {
-	TRY(8, stlink2_write_and_read_byte(pgm, 0x82, device->regs.FLASH_IAPSR) != 0x2a);
+	TRY(8, stlink2_write_and_read_byte(pgm, 0x82, device->regs.FLASH_IAPSR) & 0x4);
 }
 
 int stlink2_swim_write_range(programmer_t *pgm, stm8_device_t *device, char *buffer, unsigned int start, unsigned int length) {
