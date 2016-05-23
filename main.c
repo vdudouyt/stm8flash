df7c8be2672cccfb3f74c620b91b8bc5bd9670d2
cpldcpu <cpldcpu@gmail.com>
2016-05-23 07:49:44 +0200

    Output meaningful error message when device is not connected

--- main.c
+++ main.c
@@ -58,7 +58,7 @@ void print_help_and_exit(const char *name, bool err) {
 	fprintf(stream, "\t-r <filename>  Read data from device to file\n");
 	fprintf(stream, "\t-w <filename>  Write data from file to device\n");
 	fprintf(stream, "\t-v <filename>  Verify data in device against file\n");
-	fprintf(stream, "\t-u             Unlock. Reset option bytes to factory default.\n");
+	fprintf(stream, "\t-u             Unlock. Reset option bytes to factory default to remove write protection.\n");
 	exit(-err);
 }
 
@@ -94,7 +94,8 @@ bool usb_init(programmer_t *pgm, unsigned int vid, unsigned int pid) {
 
 	pgm->dev_handle = libusb_open_device_with_vid_pid(ctx, vid, pid);
 	pgm->ctx = ctx;
-	assert(pgm->dev_handle);
+	if (!pgm->dev_handle) spawn_error("Could not open USB device.");
+	// assert(pgm->dev_handle);
 
 	libusb_free_device_list(devs, 1); //free the list, unref the devices in it
 
